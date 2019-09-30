7be92fa7099d25aedeabfb43053b93a5e147c41a
Jiri Pech <jipech@centrum.cz>
2019-09-30 13:09:42 +0200

    update 30.9.19

--- Experiments/MicroBit/04-Poloha/Programy/3.hodina/compass.py
+++
@@ -1,9 +0,0 @@
-from microbit import *
-
-compass.calibrate()
-
-while True:
-    x = (375-compass.heading())//30
-    if (x == 12):
-        x = 0
-    display.show(Image.ALL_CLOCKS[x])
--- Experiments/MicroBit/04-Poloha/Programy/3.hodina/kompass.py
+++
@@ -1,5 +0,0 @@
-from microbit import *
-compass.calibrate()
-while True:
-       display.scroll(compass.heading())
-       sleep(1000)
